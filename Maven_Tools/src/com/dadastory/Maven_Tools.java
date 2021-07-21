b2cbe750fe52d82a9821b02df6626429c8036784
chenda <915102610106@njust.edu.cn>
2021-07-21 16:11:13 +0800

    更新配置文件，可以直接清理

--- Maven_Tools/src/com/dadastory/Maven_Tools.java
+++ Maven_Tools/src/com/dadastory/Maven_Tools.java
@@ -1,8 +1,7 @@
 package com.dadastory;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,7 +10,34 @@ import java.util.regex.Pattern;
  * @Date:2021/7/20 15:07
  */
 public class Maven_Tools {
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            File file = new File("config.properties");
+            if (file.exists()) {
+                properties.load(new FileInputStream(file));
+                System.out.println("您的配置文件如下：");
+                String JavaWorkspacePath = properties.getProperty("JavaWorkspacePath");
+                String MavenRepositoryPath = properties.getProperty("MavenRepositoryPath");
+                if (JavaWorkspacePath != null && MavenRepositoryPath != null) {
+                    System.out.println("Java项目工作路径为：" + JavaWorkspacePath);
+                    System.out.println("Maven仓库路径地址为：" + MavenRepositoryPath);
+                } else {
+                    changeProperties();
+                }
+                System.out.println("");
+            } else {
+                System.out.println("配置文件不存在！");
+                System.exit(1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         while (true) {
             System.out.println("欢迎使用Idea清理小工具");
@@ -19,7 +45,8 @@ public class Maven_Tools {
             System.out.println("-----------------------------------");
             System.out.println("1.清除Maven仓库未成功下载的POM依赖文件  |");
             System.out.println("2.清除Idea项目文件下所有的编译缓存      |");
-            System.out.println("3.退出                              |");
+            System.out.println("3.修改配置文件路径                    |");
+            System.out.println("4.退出                              |");
             System.out.println("-----------------------------------");
             System.out.println("请选择你需要的功能项：");
             String fuc = sc.nextLine();
@@ -33,18 +60,46 @@ public class Maven_Tools {
                     cleanMyProject();
                     break;
                 case 3:
+                    try {
+                        changeProperties();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 4:
                     System.exit(0);
             }
         }
     }
 
+    //修改配置路径
+    private static void changeProperties() throws IOException {
+        System.out.println("请输入新的项目工作空间的绝对路径路径:（例如：D:\\Java_workspace）");
+        String JavaWorkspacePath = sc.nextLine();
+        System.out.println("请输入新的Maven仓库文件夹的绝对路径路径:（例如：D:\\Maven\\Repository）");
+        String MavenRepositoryPath = sc.nextLine();
+        File file1 = new File(JavaWorkspacePath);
+        File file2 = new File(MavenRepositoryPath);
+        if (!file1.exists()) System.out.println("您输入的" + JavaWorkspacePath + "不存在！");
+        if (!file2.exists()) System.out.println("您输入的" + MavenRepositoryPath + "不存在！");
+        if (file1.exists() && file2.exists()) {
+            properties.setProperty("JavaWorkspacePath", JavaWorkspacePath);
+            properties.setProperty("MavenRepositoryPath", MavenRepositoryPath);
+            File file = new File("config.properties");
+            FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
+            properties.store(fileWriter, "");
+            System.out.println("配置更新成功！");
+            fileWriter.close();
+        }
+    }
+
+
     //清除项目下面的所有target编译文件
     private static void cleanMyProject() {
-        System.out.println("请输入您的项目工作空间的绝对路径路径:（例如：D:\\Java_workspace）");
-        String path = sc.nextLine();
+        String path = properties.getProperty("JavaWorkspacePath");
         File destDir = new File(path);
         if (!destDir.exists()) {
-            System.out.println("您输入的文件夹不存在，请重新输入！");
+            System.out.println("有效路径不存在，请重新配置！");
             return;
         }
         List<File> list = new ArrayList<>();
@@ -65,11 +120,10 @@ public class Maven_Tools {
 
     //清除所有pom文件
     private static void cleanMavenPom() {
-        System.out.println("请输入您的Maven仓库文件夹的绝对路径路径:（例如：D:\\Maven\\Repository）");
-        String path = sc.nextLine();
+        String path = properties.getProperty("MavenRepositoryPath");
         File destDir = new File(path);
         if (!destDir.exists()) {
-            System.out.println("您输入的文件夹不存在，请重新输入！");
+            System.out.println("有效路径不存在，请重新配置路径！");
             return;
         }
         List<File> list = new ArrayList<>();
